189a787876c068ba9dd8526e886adaaf3b082263
Kameil <130675534+Kameil@users.noreply.github.com>
2023-07-29 17:30:39 -0300

    Update autocatch3chat.py

--- autocatch3chat.py
+++ autocatch3chat.py
@@ -1,7 +1,7 @@
 import re, os, asyncio, random, string, keep_alive, random, termcolor
 from discord.ext import commands, tasks
 from termcolor import colored
-version = 'v1.3.6'
+version = 'v1.3.8'
 
 user_token = os.environ['user_token']
 catch_id = os.environ['catch_id']
