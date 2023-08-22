998bbed3daa1dd9cda6de845bf7e55e0e49daba2
Kameil <130675534+Kameil@users.noreply.github.com>
2023-08-22 14:26:19 -0300

    Atualizar o pokeones.py

--- pokeones.py
+++ pokeones.py
@@ -3,14 +3,7 @@ from discord.ext import commands, tasks
 from termcolor import colored
 
 pokeone = 473020399060385792
-if os.environ.get('prefix'):
-    osprefix = os.environ['prefix']
-    if osprefix == "":
-        prefixo = asyncio.run(osenv("prefix"))
-    else:
-        prefixo = os.environ['prefix']
-else:
-    prefixo = asyncio.run(osenv("prefix"))
+prefixo = os.environ['prefix']
 paused = False
 token = os.environ['user_token']
 chat = os.environ['chat_id']
@@ -23,7 +16,7 @@ async def analize_loop():
     fast = random.randint(3,6)
     numero = 1
     channel = client.get_channel(int(chat))
-    channel.trigger_typing()
+    channel.typing()
     await channel.send(f'{prefixo}s')
 
 @analize_loop.before_loop
@@ -41,14 +34,14 @@ async def on_message(message):
     global paused
     fast = random.randint(1,3)
     channel = client.get_channel(int(chat))
-    if message.channel.id == int(chat):
+    if message.channel.id == int(chat) and not paused:
         if message.author.id == pokeone:
             if message.embeds:
                 embed_desc = message.embeds[0].description
                 embed_title = message.embeds[0].title
                 embed_footer = message.embeds[0].footer
                 if embed_desc and 'You have won the Wild Battle!' in embed_desc:
-                    channel.trigger_typing()
+                    channel.typing()
                     await asyncio.sleep(fast)
                     dangoro = random.randint(1,10)
                     if dangoro == 5:
@@ -57,29 +50,54 @@ async def on_message(message):
                         await message.channel.send(f'{prefixo}s')
                 
                 if embed_title and 'Shiny Wild Pokémon' in embed_title:
-                    channel.trigger_typing()
+                    channel.typing()
                     await message.channel.send(f'{prefixo}master')
                 else:
                     if embed_footer and 'Send 1' in embed_footer.text:
-                        channel.trigger_typing()
+                        try:
+                            channel.typing()
+                        except:
+                            subprocess.Popen(["pip", "install", "discord.py-self==2.0.0"])
+                            print("Ocorreu um erro ao digitar.")
+                            await asyncio.sleep(30)
+                            exit()
                         await asyncio.sleep(fast)
                         await message.channel.send('1')
                 if embed_desc and "You're already in a battle." in embed_desc:
-                    channel.trigger_typing()
+                    channel.typing()
                     await asyncio.sleep(fast)
                     await channel.send('1')
             else:
                 if 'Keep the calm!' in message.content:
-                    channel.trigger_typing()
+                    channel.typing()
                     await asyncio.sleep(2)
                     await message.channel.send(f'{prefixo}s')
+    if not message.author.bot:
+        await client.process_commands(message)
 
-async def osenv(envv):
-    if envv == "prefix":
-        await asyncio.sleep(3)
-        print(colored('''Voce nao possui o secret "prefix" o AutoSpawn ira Iniciar Utilizando o prefixo Padrao "."''', "red"))
-        return "."
 
+@client.command(name='start')
+async def start_cmd(ctx):
+    if ctx.channel.id == int(chat):
+        global paused
+        await ctx.typing()
+        await asyncio.sleep(1)
+        if paused:
+            paused = False
+            await ctx.send("Bot Iniciado")
+        else:
+            await ctx.send("Bot ja esta em Execuçao.")
+        
+@client.command(name="stop")
+async def stop_cmd(ctx):
+    if ctx.channel.id == int(chat):
+        await ctx.typing()
+        await asyncio.sleep(1)
+        if not paused:
+            paused = True
+            await ctx.send("Bot pausado.")
+        else:
+            await ctx.send("Bot ja esta pausado.")
 
 async def pokeoneautospawn():
     await asyncio.sleep(1)
