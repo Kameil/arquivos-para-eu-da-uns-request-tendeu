fab868e0debb5afebe1c0ed489a9372687146a90
Kameil <130675534+Kameil@users.noreply.github.com>
2023-08-23 12:09:45 -0300

    Atualizar o pokeones.py

--- pokeones.py
+++ pokeones.py
@@ -1,4 +1,4 @@
-import re, os, asyncio, random, string, keep_alive, random
+import re, os, asyncio, random, string, keep_alive, random, time
 from discord.ext import commands, tasks
 from termcolor import colored
 
@@ -7,13 +7,21 @@ prefixo = os.environ['prefix']
 paused = False
 token = os.environ['user_token']
 chat = os.environ['chat_id']
+send_s = time.time()
 
 client = commands.Bot(command_prefix='&', help_command=None)
 esperar = [60.0, 120.0, 90.0]
 
 @tasks.loop(seconds=random.choice(esperar))
 async def analize_loop():
-    if not paused:
+    global send_s
+    agora = time.time()
+    espera = agora - send_s
+    if int(espera) >= 30:
+        pode = True
+    else:
+        pode = False
+    if not paused and pode:
         fast = random.randint(3,6)
         numero = 1
         channel = client.get_channel(int(chat))
@@ -33,6 +41,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global paused
+    global send_s
     fast = random.randint(1,3)
     channel = client.get_channel(int(chat))
     if message.channel.id == int(chat) and not paused:
@@ -49,25 +58,30 @@ async def on_message(message):
                         await message.channel.send(f'{prefixo}spawn')
                     else:
                         await message.channel.send(f'{prefixo}s')
+                    send_s = time.time()
                 
                 if embed_title and 'Shiny Wild Pokémon' in embed_title:
                     await channel.trigger_typing()
                     await message.channel.send(f'{prefixo}master')
+                    send_s = time.time()
                 else:
                     if embed_footer and 'Send 1' in embed_footer.text:
                         await channel.trigger_typing()
                         await asyncio.sleep(fast)
                         await message.channel.send('1')
+                        send_s = time.time()
                 if embed_desc and "You're already in a battle." in embed_desc:
                     await channel.trigger_typing()
                     await asyncio.sleep(fast)
                     await channel.send('1')
+                    send_s = time.time()
                     
             else:
                 if 'Keep the calm!' in message.content:
                     await channel.trigger_typing()
                     await asyncio.sleep(2)
                     await message.channel.send(f'{prefixo}s')
+                    send_s = time.time()
                     
     if message.author.id == client.user.id:
         await client.process_commands(message)
